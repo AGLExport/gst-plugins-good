597db68e14732cef66bbac17b7d05fe0945c943a
Thao Luong <thao.luong.xh@renesas.com>
2019-05-07 16:05:04 +0900

    v4l2src: Support num-alloc-buffer option
    This option uses to decide number of buffers will allocate.
    And it only supports for io-mode=mmap/dmabuf
    Default is auto decision, it will be calculated based on requirement
    from driver, proposal from downstream element and so on.
    If the value of num-alloc-buffer is too small or too big, number
    of buffers will be adjusted to suit with supported range of driver.
    
    refs #149515
    Signed-off-by: Thao Luong <thao.luong.xh@renesas.com>
    
    Porting to Gstreamr 1.18
    - Adjustment for GST_V4L2_MIN_BUFFERS no longer being a constant
    Signed-off-by: Damian Hobson-Garcia <dhobsong@igel.co.jp>

--- sys/v4l2/gstv4l2src.c
+++ sys/v4l2/gstv4l2src.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2001-2002 Ronald Bultje <rbultje@ronald.bitfreak.net>
  *               2006 Edgard Lima <edgard.lima@gmail.com>
- * Copyright (C) 2017, Renesas Electronics Corporation
+ * Copyright (C) 2019, Renesas Electronics Corporation
  *
  * gstv4l2src.c: Video4Linux2 source element
  *
@@ -67,6 +67,7 @@ GST_DEBUG_CATEGORY (v4l2src_debug);
 #define GST_CAT_DEFAULT v4l2src_debug
 
 #define DEFAULT_PROP_DEVICE   "/dev/video0"
+#define DEFAULT_NUM_ALLOC_BUF   (0xffffffff)
 
 enum
 {
@@ -76,7 +77,8 @@ enum
   PROP_CROP_TOP,
   PROP_CROP_LEFT,
   PROP_CROP_WIDTH,
-  PROP_CROP_HEIGHT
+  PROP_CROP_HEIGHT,
+  PROP_NUM_ALLOC_BUF
 };
 
 /* signals and args */
@@ -171,6 +173,13 @@ gst_v4l2src_class_init (GstV4l2SrcClass * klass)
       g_param_spec_int ("crop-height", "Height size",
           "Height of the CROP area. 0: Equal with input height",
           0, G_MAXINT, 0, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+  g_object_class_install_property (gobject_class, PROP_NUM_ALLOC_BUF,
+      g_param_spec_uint ("num-alloc-buffer",
+          "Number of buffer request to driver",
+          "Number of buffers will allocate (support for mmap and dmabuf io-mode)"
+          "If the number is out of support of driver, it will be adjusted (0xffffffff=auto)",
+          0, G_MAXUINT, DEFAULT_NUM_ALLOC_BUF,
+          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   /**
    * GstV4l2Src::prepare-format:
@@ -230,6 +239,8 @@ gst_v4l2src_init (GstV4l2Src * v4l2src)
 
   gst_base_src_set_format (GST_BASE_SRC (v4l2src), GST_FORMAT_TIME);
   gst_base_src_set_live (GST_BASE_SRC (v4l2src), TRUE);
+
+  v4l2src->num_alloc_buffer = DEFAULT_NUM_ALLOC_BUF;
 }
 
 
@@ -263,6 +274,9 @@ gst_v4l2src_set_property (GObject * object,
       case PROP_CROP_HEIGHT:
         v4l2src->crop.height = g_value_get_int (value);
         break;
+      case PROP_NUM_ALLOC_BUF:
+        v4l2src->num_alloc_buffer = g_value_get_uint (value);
+        break;
       default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
         break;
@@ -290,6 +304,9 @@ gst_v4l2src_get_property (GObject * object,
         break;
       case PROP_CROP_HEIGHT:
         g_value_set_int (value, v4l2src->crop.height);
+        break;
+      case PROP_NUM_ALLOC_BUF:
+        g_value_set_uint (value, v4l2src->num_alloc_buffer);
         break;
       default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
